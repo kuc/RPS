0292d7e2dfdd4d9e95a87192984d5cdec094c49f
kuc <49013038+kuc@users.noreply.github.com>
2022-03-31 20:34:44 -0500

    function descriptions

--- RPS.cpp
+++ RPS.cpp
@@ -8,10 +8,19 @@ using namespace std;
 
 
 
-// Throw
+// Throw Constructor
 Throw::Throw() {
 	options = 3;
 }
+
+//***********************************************************
+// 
+// Shoot() picks a random number in the range 0-2
+// 
+// Sent in: Nothing
+// Sent back: a number from 0-2
+// 
+//***********************************************************
 int Throw::Shoot() {
 	return (rand() % options); 
 
@@ -20,53 +29,87 @@ int Throw::Shoot() {
 	// 2 = rock
 }
 
-// Player
+// Player Constructor
 Player::Player() {
 	name = " ";
 }
 
+// Player Value Constructor
 Player::Player(string n) {
 	name = n;
 }
 
+// Player Deconstructor
 Player::~Player() {
 	if (throws)
 		delete[] throws;
 }
 
-//ostream& Player::Display(ostream& out) const {
+//***********************************************************
+// 
+// Display() shows the players name, what they threw each round,
+// if they even played, and their score
+// 
+// Sent in: implicit
+// Sent back: Nothing
+// 
+//***********************************************************
 void Player::Display(ostream& out) const {
-	out << "Players Name is: " << name << endl;
+	out << "Player's Name is: " << name << endl;
 	if (throws != nullptr) {
 		for (int i = 0; i < 3; i++) {
 			if (throws[i] == 0) {
-				out << "Round " << i << ":" << " Paper" << endl;
+				out << "Throw " << i+1 << ":" << " Paper" << endl;
 			}
 			if (throws[i] == 1) {
-				out << "Round " << i << ":" << " Scissors" << endl;
+				out << "Throw " << i+1 << ":" << " Scissors" << endl;
 			}
 			if (throws[i] == 2) {
-				out << "Round " << i << ":" << " Rock" << endl;
+				out << "Throw " << i+1 << ":" << " Rock" << endl;
 			}
 		}
 	}
 	else
 		out << "Player " << name << " has played no games yet." << endl;
-	out << "There score is: " << score << endl;
+	out << name << "'s score is: " << score << endl;
 // Display rock,paper,scissors
 // display score
 
 }
 
+//***********************************************************
+// 
+// Score() shows the score of the player
+// 
+// Sent in: Nothing
+// Sent back: players score
+// 
+//***********************************************************
 int Player::Score() const {
 	return score;
 }
 
+//***********************************************************
+// 
+// Name() shows the name of the player
+// 
+// Sent in: Nothing
+// Sent back: the name of the player
+// 
+//***********************************************************
 string Player::Name() const
 {
 	return name;
 }
 
+//***********************************************************
+// 
+// TakeTurn() fills the array throw by calling the function Shoot() to fill each index
+// 
+// Sent in: Nothing
+// Sent back: Nothing
+// 
+//***********************************************************
 void Player::TakeTurn()
 {
 	Throw *t = new Throw;
@@ -75,6 +118,15 @@ void Player::TakeTurn()
 	}
 }
 
+//***********************************************************
+// 
+// Match() figures out which player has the highest score at
+// the end of all three rounds
+// 
+// Sent in: implicit and p2
+// Sent back: which player won or tied
+// 
+//***********************************************************
 string Player::Match(Player &p2)
 {
 	int p1s = 0;
@@ -117,6 +169,15 @@ string Player::Match(Player &p2)
 	}
 }
 
+//***********************************************************
+// 
+// operator = makes sure each part of the Player class is
+// copied over
+// 
+// Sent in: implicit and right
+// Sent back: implicit
+// 
+//***********************************************************
 const Player& Player::operator =(const Player& right)
 {
 	if (this != &right) {
@@ -131,12 +192,27 @@ const Player& Player::operator =(const Player& right)
 	return * this;
 }
 
-
+//***********************************************************
+// 
+// operator < compares the score of two players
+// 
+// Sent in: left and right Player class
+// Sent back: true or false
+// 
+//***********************************************************
 bool operator<(const Player& left, const Player& right)
 {
 	return (left.Score() < right.Score());
 }
 
+//***********************************************************
+// 
+// operator << hides a call to the Display function
+// 
+// Sent in: Player class
+// Sent back: out
+// 
+//***********************************************************
 ostream& operator<<(ostream& out, const Player& player)
 {
 	player.Display(out);
