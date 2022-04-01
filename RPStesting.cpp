3e7de473b5e485f3e3eab21f52fd02fe1960755f
kuc <49013038+kuc@users.noreply.github.com>
2022-03-31 20:36:38 -0500

    main loop, end scoring, bad integer input for players, added round counter, move active to inactive

--- RPStesting.cpp
+++ RPStesting.cpp
@@ -1,3 +1,14 @@
+/* 
+   Rock Paper Scissors
+
+   Programmer: Kuc
+
+   Purpose: This program is a game of rock, paper, scissors. It will ask the user
+   how many players there will be and to name them. It will play as many rounds
+   as it needs to until there is only one player left. At the end it will display
+   the winner which is the player who has the highest score. 
+*/
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
@@ -10,19 +21,16 @@ using namespace std;
 
 int getPlayer();
 void AssignPlayer(int players, Player*& active);
-void Play(Player*& active, Player*& inactive, int& PlayersRemain, int& PlayersInactive, int player);
+void Play(Player*& active, Player*& inactive, int& PlayersRemain, int& PlayersInactive, int& round);
+void ScoreMe(Player* active, Player* inactive, int PlayersInactive);
 
 int main() {
-	// A random generator seed using time
-	//unsigned seed = time(0);
-	// Set the random generator seed before calling rand()
-	//srand(seed);
-	//int num1 = rand();
 
 	int player;
 	player = getPlayer();
 	int PlayersRemain = player;
 	int PlayersInactive = 0;
+	int whatround = 1; 
 
 	Player* active = new Player[player];
 	if (!active) {
@@ -38,13 +46,14 @@ int main() {
 
 	AssignPlayer(player, active);
 
-	//while (PlayersRemain > 1) {
-	//	// call play function
-	//}
+	while (PlayersRemain > 1) {
+		Play(active, inactive, PlayersRemain, PlayersInactive, whatround);
+	}
 
-	cout << player << endl;
-	Play(active, inactive, PlayersRemain, PlayersInactive, player);
+	ScoreMe(active, inactive, PlayersInactive);
 
+	cout << "Press Any Key to Continue..." << endl;
+	exit(0);
 	return 0;
 }
 
@@ -52,8 +61,20 @@ int main() {
 
 int getPlayer() {
 	int temp;
-	cout << "How many players do you want?: " << endl;
-	cin >> temp;
+	bool valid = false;
+	// make sure integer is inputted
+	while (!valid) {
+		cout << "How many players do you want?: ";
+		cin >> temp;
+		if (cin.fail()) {
+			cout << "Wrong input!" << endl;
+
+			cin.clear();
+			cin.ignore(INT_MAX, '\n');
+		}
+		else
+			valid = true;
+	}
 	return temp;
 }
 
@@ -69,7 +90,7 @@ void AssignPlayer(int players, Player*& active) {
 
 }
 
-void Play(Player*& active, Player*& inactive, int& PlayersRemain, int& PlayersInactive, int const player) {
+void Play(Player*& active, Player*& inactive, int& PlayersRemain, int& PlayersInactive, int& round) {
 	unsigned seed = time(0);
 	srand(seed);
 
@@ -85,15 +106,19 @@ void Play(Player*& active, Player*& inactive, int& PlayersRemain, int& PlayersIn
 	active[player1].TakeTurn();
 	active[player2].TakeTurn();
 
-	//cout << active[player1] << endl;
-	//cout << active[player2] << endl;
+	cout << "********** Round " << round << " **********" << endl;
 
 	string match = active[player1].Match(active[player2]);
 
 	if (match == "p1") {
-		cout << "Player " << active[player1].Name() << " Wins!" << endl;;
+		cout << "Player " << active[player1].Name() << " Wins!" << endl << endl;
 		cout << active[player1] << endl;
+		cout << active[player2] << endl;
 		PlayersInactive++;
+
+		if (PlayersRemain > 1) {
+			inactive[PlayersInactive - 1] = active[player2];
+		}
 
 		if (player2 < 0 || player2 >= PlayersRemain) {
 			cerr << "Illegal Pos - Cannot erase" << endl;
@@ -107,11 +132,14 @@ void Play(Player*& active, Player*& inactive, int& PlayersRemain, int& PlayersIn
 		// move p2 to inactive
 	}
 	if (match == "p2") {
-		cout << "Player " << active[player2].Name() << " Wins!" << endl;;
+		cout << "Player " << active[player2].Name() << " Wins!" << endl << endl;
+		cout << active[player1] << endl;
 		cout << active[player2] << endl;
 		PlayersInactive++;
 
-
+		if (PlayersRemain > 1) {
+			inactive[PlayersInactive - 1] = active[player1];
+		}
 
 		if (player1 < 0 || player1 >= PlayersRemain) {
 			cerr << "Illegal Pos - Cannot erase" << endl;
@@ -125,9 +153,25 @@ void Play(Player*& active, Player*& inactive, int& PlayersRemain, int& PlayersIn
 		// move p1 to inactive
 	}
 	if (match == "tie") {
-		cout << "Player " << active[player1].Name() << " and " << active[player2].Name() << " tied!" << endl;;
+		cout << "Player " << active[player1].Name() << " and " << active[player2].Name() << " tied!" << endl << endl;
 		cout << active[player1] << endl;
 		cout << active[player2] << endl;
 		// dont move anyone to inactive
 	}
+
+	round++;
+}
+
+void ScoreMe(Player* active, Player* inactive, int PlayersInactive) {
+	string maxname = active[0].Name();
+	int maxscore = active[0].Score();
+
+	for (int i = 0; i < PlayersInactive; i++) {
+		if (inactive[i].Score() > maxscore) {
+			maxscore = inactive[i].Score();
+			maxname = inactive[i].Name();
+		}
+	}
+
+	cout << "The winner with a score of " << maxscore << " is " << maxname << endl;
 }
